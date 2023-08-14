b6c7f6ff9e8dcd42d31fdb1320019ae228404a60
Mustafa Yuksel <mryuksel@gmail.com>
2023-08-15 02:21:46 +0300

    update

--- parse.py
+++ parse.py
@@ -5,12 +5,44 @@ root = ET.fromstring(xml_text)
 simple_dictionary = {}
 simple_dictionary["words"] = []
 entry_elements = root.findall(".//{http://www.tei-c.org/ns/1.0}entry")
-"""
-orth_element = root.find(".//orth")
-pos_element = root.find(".//pos")
-simple_dictionary["words"].append({})
-simple_dictionary["words"][-1]["word"] = orth_element.text
+index = 0
+wordName = "deneme"
+def addPolysemanticKeyToArray():
+    simple_dictionary["words"][-1]["polysemantic"] = []
+def increaseTheIndex():
+    global index  # global değişkeni kullanacağımızı belirtiyoruz
+    index = index + 1
+def getNextIndex():
+    nextIndex = index + 1
+    return nextIndex
+def addEmtyMaptoArray():
+    simple_dictionary["words"].append({})
+def getWordName(entry):
+    wordName =  entry.find(".//{http://www.tei-c.org/ns/1.0}orth").text
+    print(wordName)
+    simple_dictionary["words"][-1]["word"] = wordName
+def getNextWordName():
+    nextEntry = entry_elements[getNextIndex()]
+    nextwordName =  nextEntry.find(".//{http://www.tei-c.org/ns/1.0}orth").text
+    return nextwordName
+
+for entry in entry_elements:
+    addEmtyMaptoArray()
+    getWordName(entry)
+    """
+    bazı kelimeler aynı yazıldığı halde birden çok anlama sahip oluyor.
+    bundan bir sonraki index'teki elementin ismi bu index'teki elementin ismiyle uyuşuyor mu kontrol et.
+    Eğer uyuşuyor ise kelimenin birden fazla anlamı var demektir
+    """
+    if wordName == getNextWordName() : 
+        print("kelimenin birden fazla anlamı var")
+        addPolysemanticKeyToArray()
+    else:
+        print("kelimenin birden fazla anlamı yok")    
+    
+    increaseTheIndex()
+orth_element = root.find(".//{http://www.tei-c.org/ns/1.0}orth")
+pos_element = root.find(".//{http://www.tei-c.org/ns/1.0}pos")
 simple_dictionary["words"][-1]["type"] = pos_element.text
 simple_dictionary["words"][-1]["polysemantic"] = pos_element.text
-"""
-print(entry_elements)
+print(simple_dictionary)
